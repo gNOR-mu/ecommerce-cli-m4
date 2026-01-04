bf9302681493e7dc34d4f8c8d6b441aae1a89577
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-01-04 18:28:22 -0300

    actualización  añadida

--- src/main/java/com/ecommerce/demo/view/admin/ProductManagerMenu.java
+++ src/main/java/com/ecommerce/demo/view/admin/ProductManagerMenu.java
@@ -48,7 +48,7 @@ public class ProductManagerMenu extends AbstractMenu {
             case 1 -> findAll();
             case 2 -> search();
             case 3 -> create();
-            case 4 -> edit();
+            case 4 -> update();
             case 5 -> delete();
             default -> System.out.println("Opción inválida");
         }
@@ -65,17 +65,14 @@ public class ProductManagerMenu extends AbstractMenu {
     }
 
     public void create() {
-        System.out.println("Categorías disponibles: ");
-        for (Category category : categoryService.findAll()) {
-            System.out.println(category);
-        }
+       List<Category> categories = categoryService.findAll();
 
-        System.out.println();
         String name = inputHandler.readText("Ingresa el nombre del producto: ");
+
+        FormatUtil.printCategories(categories);
         Long categoryId = inputHandler.readLong("Ingresa la id de la categoría: ");
         BigDecimal price = inputHandler.readBigDecimal("Ingresa el precio del producto: ");
         Product product = new Product(categoryId, price, name);
-        //TODO pensar si es necesario permitir o no un valor negativo
         int stock = inputHandler.readInt("Cantidad de stock: ");
 
         try {
@@ -85,8 +82,32 @@ public class ProductManagerMenu extends AbstractMenu {
         }
     }
 
-    private void edit() {
+    private void update() {
+        long id = inputHandler.readLong("Ingresa la id del producto a editar: ");
 
+        //búsqueda del producto actual
+        try {
+
+            ProductSummaryDto product = productService.getSummaryById(id);
+            List<Category> categories = categoryService.findAll();
+            FormatUtil.printProductSummary(product);
+
+            //modificaciones
+            System.out.println("Presiona enter para dejar el campo sin modificar");
+            String name = inputHandler.editString("Producto", product.name());
+
+            FormatUtil.printCategories(categories);
+            Long categoryId = inputHandler.editLong("Categoría", product.categoryId());
+            BigDecimal price = inputHandler.editBigDecimal("Precio", product.price());
+            int stock = inputHandler.editInt("Stock", product.stock());
+
+            //actualización
+            Product updatedProduct = new Product(categoryId, price, name);
+            productService.update(id, updatedProduct, stock);
+            System.out.println("Producto actualizado");
+        } catch (Exception e) {
+            System.out.println("No se ha actualizado el producto debido a que: " + e.getMessage());
+        }
     }
 
     private void delete() {
