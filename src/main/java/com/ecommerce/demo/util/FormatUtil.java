336be11cdbb8d4d9fc394ef0ce63ecdf76e920ae
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-01-04 18:30:04 -0300

    método de formato para categoría añadido y modificaciones al imprimir campos sin elementos

--- src/main/java/com/ecommerce/demo/util/FormatUtil.java
+++ src/main/java/com/ecommerce/demo/util/FormatUtil.java
@@ -1,12 +1,16 @@
 package com.ecommerce.demo.util;
 
 import com.ecommerce.demo.dto.ProductSummaryDto;
+import com.ecommerce.demo.model.Category;
 
 import java.util.List;
 
 public class FormatUtil {
     private static final String PRODUCT_SUMMARY_FORMAT = "| %-4s | %-20s | %-15s | %10s | %5s |%n";
-    private static final String SEPARATOR = "+------+----------------------+-----------------+------------+-------+";
+    private static final String PRODUCT_BORDER = "+------+----------------------+-----------------+------------+-------+";
+
+    private static final String CATEGORY_FORMAT = "| %-4s | %-30s |%n";
+    private static final String CATEGORY_BORDER = "+------+--------------------------------+";
 
     public static String truncateText(String text, int maxSize) {
         //así evito que se rompa la tabla
@@ -16,10 +20,18 @@ public class FormatUtil {
     }
 
     public static void printProductSummary(List<ProductSummaryDto> products) {
-        // Imprimir Cabecera
-        System.out.println(SEPARATOR);
-        System.out.printf(PRODUCT_SUMMARY_FORMAT, "ID", "NOMBRE", "CATEGORÍA", "PRECIO", "STOCK");
-        System.out.println(SEPARATOR);
+        if (products.isEmpty()) {
+            System.out.println("""
+                    +-------------------------------------+
+                    |  ️  No hay productos registrados.     |
+                    +-------------------------------------+
+                    """);
+            return;
+        }
+
+        System.out.println(PRODUCT_BORDER);
+        System.out.printf(PRODUCT_SUMMARY_FORMAT, "ID", "PRODUCTO", "CATEGORÍA", "PRECIO", "STOCK");
+        System.out.println(PRODUCT_BORDER);
 
         // Imprimir Filas
         for (ProductSummaryDto product : products) {
@@ -31,11 +43,43 @@ public class FormatUtil {
                     product.stock());
         }
 
-        // Imprimir Pie
-        System.out.println(SEPARATOR);
+        System.out.println(PRODUCT_BORDER);
     }
 
     public static void printProductSummary(ProductSummaryDto product) {
         printProductSummary(List.of(product));
+    }
+
+    public static void printCategories(List<Category> categories) {
+        if (categories.isEmpty()) {
+            System.out.println("""
+                    +-------------------------------------+
+                    |  ️  No hay categorías registradas.    |
+                    +-------------------------------------+
+                    """);
+            System.out.println(CATEGORY_BORDER);
+            System.out.println("|  ️  No hay categorías registradas.   |");
+            System.out.println(CATEGORY_BORDER);
+            return;
+        }
+
+        System.out.println(CATEGORY_BORDER);
+        System.out.printf(CATEGORY_FORMAT, "ID", "CATEGORÍA");
+        System.out.println(CATEGORY_BORDER);
+
+        for (Category c : categories) {
+            System.out.printf(CATEGORY_FORMAT,
+                    c.getId(),
+                    truncateText(c.getName(), 30)
+            );
+        }
+
+        // Cierre
+        System.out.println(CATEGORY_BORDER);
+    }
+
+
+    public static void printCategory(Category category) {
+        printCategories(List.of(category));
     }
 }
