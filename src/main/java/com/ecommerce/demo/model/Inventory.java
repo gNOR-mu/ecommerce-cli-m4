8cfe96f7084fdf84747edac7b492d59dedc58b02
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-01-04 15:33:27 -0300

    tostring añadido

--- src/main/java/com/ecommerce/demo/model/Inventory.java
+++ src/main/java/com/ecommerce/demo/model/Inventory.java
@@ -38,4 +38,13 @@ public class Inventory implements Identifiable<Long> {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    @Override
+    public String toString() {
+        return "Inventory{" +
+                "id=" + id +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
