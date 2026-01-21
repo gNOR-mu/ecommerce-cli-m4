af0977eabc6561da1dc4f71071148da1e8da6607
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-01-21 18:52:54 -0300

    eliminados métodos sin usar

--- src/main/java/com/ecommerce/demo/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/demo/repository/ProductRepository.java
@@ -17,32 +17,4 @@ import java.util.List;
  */
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    /**
-     * Recupera una lista de productos cuyo nombre contiene el texto proporcionado ignorando el caso.
-     * <p>
-     * La búsqueda no debe distinguir entre mayúsculas y minúsculas (case-insensitive).
-     * Si el término de búsqueda es vacío, el comportamiento depende de la implementación.
-     * </p>
-     *
-     * @param name El texto parcial o completo a buscar en el nombre del producto.
-     * No debe ser nulo.
-     * @return Una lista inmutable de productos que coinciden con el criterio.
-     * Devuelve una lista vacía si no se encuentran coincidencias.
-     * Nunca devuelve {@code null}.
-     */
-    List<Product> findByNameIgnoreCase(String name);
-
-    /**
-     * Recupera una lista de productos cuya id de la categoría coincide con la identificación de categoría proporcionada.
-     * <p>
-     * La búsqueda debe ser exacta con la id de categoría.
-     * </p>
-     *
-     * @param categoryId id de la categoría a buscar.
-     * No debe ser nulo.
-     * @return Una lista inmutable de productos que coinciden con el criterio.
-     * Devuelve una lista vacía si no se encuentran coincidencias.
-     * Nunca devuelve {@code null}.
-     */
-    List<Product> findByCategoryId(Long categoryId);
 }
--- src/main/java/com/ecommerce/demo/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/ecommerce/demo/repository/impl/InMemoryProductRepository.java
@@ -3,7 +3,6 @@ package com.ecommerce.demo.repository.impl;
 import com.ecommerce.demo.model.Product;
 import com.ecommerce.demo.repository.ProductRepository;
 
-import java.util.List;
 
 /**
  * Implementación en memoria de {@link ProductRepository}
@@ -12,39 +11,4 @@ import java.util.List;
  */
 public class InMemoryProductRepository extends InMemoryAbstractRepository<Product> implements ProductRepository {
 
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <b>Nota de implementación:</b>
-     * Realiza un filtrado secuencial sobre todos los elementos en memoria.
-     * Se retorna una lista vacía si name es null o isBlank es true
-     * </p>
-     */
-    @Override
-    public List<Product> findByNameIgnoreCase(String name) {
-        if (name == null || name.isBlank()){
-            return List.of();
-        }
-        return database.values().stream()
-                .filter(product -> product.getName().equalsIgnoreCase(name))
-                .toList();
-    }
-
-    /**
-     * {@inheritDoc}
-     * <p>
-     * <b>Nota de implementación:</b>
-     * Realiza un filtrado secuencial sobre todos los elementos en memoria.
-     * Se retorna una lista vacía si categoryId es null
-     * </p>
-     */
-    @Override
-    public List<Product> findByCategoryId(Long categoryId) {
-        if (categoryId == null){
-            return List.of();
-        }
-        return database.values().stream()
-                .filter(product -> product.getCategoryId().equals(categoryId))
-                .toList();
-    }
 }
